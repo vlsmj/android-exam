7938e3b77b4768cd39758c7e8ea335f10e2fc025
Van Javier <vanlaurence08@gmail.com>
2021-06-28 23:15:28 +0800

    implement list of persons

--- common/src/main/java/com/vanjavier/common/payload/PersonsPayload.kt
+++ common/src/main/java/com/vanjavier/common/payload/PersonsPayload.kt
@@ -3,6 +3,6 @@ package com.vanjavier.common.payload
 import com.vanjavier.common.entities.Person
 
 data class PersonsPayload(
-    val token: String? = "tTCg5emLIqSIh5BzxKl6ag",
+    val token: String? = "Dz7W4GZlWKsJe0hGfxvVOQ",
     val data: Person? = Person()
 )
